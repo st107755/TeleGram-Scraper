29128fd660363b7684c6e5ae705787cc5c482e15
Your Name <you@example.com>
2022-04-13 20:42:07 +0200

    ner commends for maike

--- ner.py
+++ ner.py
@@ -5,13 +5,16 @@ import ipdb
 
 # load tagger
 tagger = SequenceTagger.load("flair/ner-german-large")
+# read data
 df = pd.read_csv('chat.csv',delimiter=';')
-df = df.dropna()
+# will empty with string
+df = df.fillna('')
 classified_sampes = 0
 for index, row in df.iterrows():
+    # classify sentence
     sentence = Sentence(row['text'])
     tagger.predict(sentence)
-    
+    # search for location in classification
     for entitys in sentence.get_spans('ner'):
         if entitys.tag == 'LOC':
             classified_sampes += 1
