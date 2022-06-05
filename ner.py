46d8df9f87d05823de25087faa415815e08575d5
nm <m@example.com>
2022-06-05 19:54:00 +0200

    dataframe to geo dataframe with points

--- ner.py
+++ ner.py
@@ -1,7 +1,10 @@
 from flair.data import Sentence
 from flair.models import SequenceTagger
 import pandas as pd
-import ipdb
+import text2geo #file with geofunction
+import geopandas as gpd
+from shapely.geometry import Point
+import pdb
 
 # load tagger
 tagger = SequenceTagger.load("flair/ner-german-large")
@@ -9,19 +12,22 @@ tagger = SequenceTagger.load("flair/ner-german-large")
 df = pd.read_csv('chat.csv',delimiter=';')
 # will empty with string
 df = df.fillna('')
-classified_sampes = 0
+# gdf = gpd.GeoDataFrame(df)
+
 for index, row in df.iterrows():
     # classify sentence
     sentence = Sentence(row['text'])
+    entity_list = []
     tagger.predict(sentence)
     # search for location in classification
     for entitys in sentence.get_spans('ner'):
-        if entitys.tag == 'LOC':
-            classified_sampes += 1
-            print(entitys)
-            break
-    
-print("Totals number of recongnised locations")
-print(classified_sampes)
-    
-    # Test 2,3, 4,5
+          if entitys.tag == 'LOC':
+            entity_list.append(entitys.text)
+    # pdb.set_trace()
+    df.loc[index,'geometry'] = text2geo.text2geo(entity_list).wkt
+    # pdb.set_trace()
+
+df['geometry'] = gpd.GeoSeries.from_wkt(df['geometry'])
+gdf = gpd.GeoDataFrame(df, geometry='geometry')
+gdf.to_csv('chat_geo.csv', sep=';', encoding='utf-8-sig', index=False) 
+
