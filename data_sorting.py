2a608175d36e127ac3db2ffb3ed6990059cdccf7
Maike Gräff <mgraeff@a6-vasquez-mbp.local>
2022-06-24 23:31:38 +0200

    finished Method to extract dates and times from chats

--- data_sorting.py
+++ data_sorting.py
@@ -4,58 +4,53 @@ from datetime import date, time, datetime
 import re
 import pdb
 
-data = pd.read_csv('chat.csv',delimiter=';')
+data_raw = pd.read_csv('chat.csv',delimiter=';')
 
-
-
-format_string_dt = "%Y-%m-%d %H:%M:%S+00:00"
-format_string_time = "%H:%M Uhr"
-
-date_string = "2022-04-14 12:43:22+00:00"
-
-
-
-
-#extract important parts
-td = data [["text","date"]]
+#extract important parts Chat1
+td = data_raw [["text","date"]]
 td.dropna(inplace=True)
 td.drop(td.tail(1).index,inplace=True)
 split_text=td['text'].str.split('-',expand=True)
-df= split_text.join(td["date"])
-
-#print(df)
-
-for index, row in df.iterrows():
-    df_date = df.loc[index,'date']
-    #
-    #df_time = df.loc[index,0]
-    df_time = re.search('\d\d:\d\d',df.loc[index,0]).group()
-    df_list = df_time.split(':')
-    #pdb.set_trace()
-
-    dt_date = datetime.strptime(df_date, format_string_dt)
-    dt_datetime = dt_date.replace(hour=int(df_list[0]),minute=int(df_list[1]),second=0)
-    print("date")
-    print(dt_date)
-    print("new")
-    print(dt_datetime)
+df1= split_text.join(td["date"])
 
 
-def detect_time (date,clock):
-    pass
+format_string_dt = "%Y-%m-%d %H:%M:%S+00:00"
+
+#Method to extract the dates and times from chat1 and extend dataframe
+def detect_time_chat1 (df1,format_string_dt):
+    dt_column = []
+    for index, row in df1.iterrows():
+        tmp_time = []
+        df_date = df1.loc[index,'date']
+        tmp_time = re.search('\d\d:\d\d',df1.loc[index,0])
+        
+        if tmp_time is None:
+            dt_datetime = ""
+        else:
+            df_time = tmp_time.group()
+            df_list = df_time.split(':')
+            dt_date = datetime.strptime(df_date, format_string_dt)
+            dt_datetime = dt_date.replace(hour=int(df_list[0]),minute=int(df_list[1]),second=0)
+            
+        dt_column.append (dt_datetime)
+
+    df1 ["datetime"] = dt_column
+    return df1
+
+df = detect_time_chat1 (df1,format_string_dt)
+print (df)
+
 
 ##Methode detect_date fertig machen
 #Ausnahmen beheben
 
 ## dataframe erstellen: Spalten: date, type(Blitzer,...), location
-
-
+#neuer dataframe?
 
 
 
 
 #Method for detecting street names
-
 
 
 #Sorting comments into new files depending on the presence of a street name
